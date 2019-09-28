fe68a277ddc01b0833d43b2c2c582d8d4bf73ba3
thanatos9516 <chepe352013@gmail.com>
2019-09-28 01:11:09 -0600

    Update EmpleadoService.java

--- src/main/java/com/example/demo/service/EmpleadoService.java
+++ src/main/java/com/example/demo/service/EmpleadoService.java
@@ -2,6 +2,8 @@ package com.example.demo.service;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -21,31 +23,44 @@ public class EmpleadoService {
 	@Qualifier("convertidor")
 	private Convertidor convertidor;
 	
+	private static final Log logger = LogFactory.getLog(EmpleadoService.class);
+	
 	public boolean add(Empleado empleado) {
+		logger.info("CREANDO EMPLEADO");
 		try {
 			repositorio.save(empleado);
+			logger.info("EMPLEADO CREADO");	
 			return true;
 		}catch(Exception e) {
+			
+			logger.error("HUBO UN ERROR");
 			return false;
 		}
 	}
 	
 	public boolean update(Empleado empleado) {
+		logger.info("ACTUALIZANDO EMPLEADO");
 		try {
 			repositorio.save(empleado);
+			logger.info("EMPLEADO ACTUALIZADO");
 			return true;
 		}catch(Exception e) {
+			
+			logger.error("HUBO UN ERROR");
 			return false;
 		}
 	}
 	
 	public boolean delete(String nombre, long id) {
+		logger.warn("ELIMINANDO EMPLEADO");
 		try {
 			Empleado empleado = repositorio.findByNombreAndId(nombre, id);
 			
 			repositorio.delete(empleado);
+			logger.info("EMPLEADO ELIMINADO");
 			return true;
 		}catch(Exception e) {
+			logger.error("HUBO UN ERROR");
 			return false;
 		}
 	}
