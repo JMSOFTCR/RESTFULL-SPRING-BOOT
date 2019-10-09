e44b2b55bad09961946ec00557ad98a833778440
thanatos9516 <chepe352013@gmail.com>
2019-10-08 23:00:11 -0600

    Token update

--- src/main/java/com/example/demo/configuration/LoginFilter.java
+++ src/main/java/com/example/demo/configuration/LoginFilter.java
@@ -1,21 +1,60 @@
 package com.example.demo.configuration;
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.demo.configuration.JwtUtil;
+import com.example.demo.configuration.User;
+
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
 
 public class LoginFilter extends AbstractAuthenticationProcessingFilter {
-	
-	public LoginFilter(String url, AuthenticationManager authManager) {
-		super(new AntPathRequestMatcher(url));
-		setAuthenticationManager(authManager);
-	}
-	
-	@Override
-	public Authentication attemptAuthentication()
 
+    public LoginFilter(String url, AuthenticationManager authManager) {
+        super(new AntPathRequestMatcher(url));
+        setAuthenticationManager(authManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(
+            HttpServletRequest req, HttpServletResponse res)
+            throws AuthenticationException, IOException, ServletException {
+        
+        InputStream body = req.getInputStream();
+
+        User user = new ObjectMapper().readValue(body, User.class);
+
+        return getAuthenticationManager().authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        user.getUser(),
+                        user.getPassword(),
+                        Collections.emptyList()
+                )
+        );
+    }
+
+    @Override
+    protected void successfulAuthentication(
+            HttpServletRequest req,
+            HttpServletResponse res, FilterChain chain,
+            Authentication auth) throws IOException, ServletException {
+
+        // Si la autenticacion fue exitosa, agregamos el token a la respuesta
+        JwtUtil.addAuthentication(res, auth.getName());
+    }
 }
+
 
 //Revisar todo los import, están diferentes al video 13 minuto 3:45
